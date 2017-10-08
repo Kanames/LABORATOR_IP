f93703779bccf2328dd44a636ed6089b2bc1a6fc
Asus Home <Asus Home@DESKTOP-CGDSSVT>
2017-10-08 20:16:46 +0300

    etapa 02 lab 01

--- LABORATOR 01/lab_01_temaPatrat/MoveSquare.java
+++ LABORATOR 01/lab_01_temaPatrat/MoveSquare.java
@@ -9,6 +9,8 @@ import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
+
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Component;
@@ -17,6 +19,7 @@ public class MoveSquare {
 
 	private JFrame frame;
 	private int nr_clickVar;
+	Random rand = new Random();
 
 	/**
 	 * Launch the application.
@@ -54,30 +57,74 @@ public class MoveSquare {
 		playingGround.setLayout(null);
 		
 		JLabel lblClicked = new JLabel("Clicked:");
-		lblClicked.setBounds(10, 236, 46, 14);
+		lblClicked.setBounds(10, 215, 46, 14);
 		playingGround.add(lblClicked);
 		
 		final JLabel nr_clicks = new JLabel("");
-		nr_clicks.setBounds(66, 236, 46, 14);
+		nr_clicks.setBounds(66, 215, 46, 14);
 		playingGround.add(nr_clicks);
+		
+		final JLabel lblWidth = new JLabel("Width:");
+		lblWidth.setBounds(288, 215, 46, 14);
+		playingGround.add(lblWidth);
+		
+		final JLabel lblHeight = new JLabel("Height:");
+		lblHeight.setBounds(288, 236, 46, 14);
+		playingGround.add(lblHeight);
+		
+		final JLabel width = new JLabel("");
+		width.setBounds(344, 215, 46, 14);
+		playingGround.add(width);
+		
+		final JLabel height = new JLabel("");
+		height.setBounds(344, 236, 46, 14);
+		playingGround.add(height);
 		
 		nr_clickVar = 0;
 		
 		final Canvas canvas = new Canvas();
+		canvas.setBounds(10, 10, 414, 199);
+		
+		final int width_var = canvas.getWidth();
+		final int height_var = canvas.getHeight();
+		System.out.println("width_var: "+canvas.getWidth());
+		System.out.println("height_var: "+canvas.getHeight());
+		
+		width.setText(Integer.toString(width_var));
+		height.setText(Integer.toString(height_var));
+		
 		canvas.setBackground(Color.LIGHT_GRAY);
 		canvas.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				
 				nr_clickVar++;
 				nr_clicks.setText(Integer.toString(nr_clickVar));
-				 Graphics g = canvas.getGraphics();
-			     g.fillRect(10, 10, 25, 25);
-			     
+				
+				Graphics g = canvas.getGraphics();
+				System.out.println("in afara for-ului...");
+				
+				for (int i=0; i < 100; i++) {
+					for (int j=0; j< 100; j++) {
+						canvas.paint(g);
+						g.fillRect(rand.nextInt(width_var) + i, rand.nextInt(height_var) + j, 25, 25);
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				}
+				
+				
+//				 g.clearRect(width_var+nr_clickVar,hright_var+nr_clickVar, 25, 25);
+//				 g.drawRect(width_var+nr_clickVar,hright_var+nr_clickVar, 25, 25);
+				 
 			}
 		});
-		canvas.setBounds(10, 10, 414, 199);
-		playingGround.add(canvas);
 		
+		playingGround.add(canvas);
+
 		
 	}
 	 
