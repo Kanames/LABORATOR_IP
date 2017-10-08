d107ed1c5a49d2785115a6a2d4045748072f961b
Asus Home <Asus Home@DESKTOP-CGDSSVT>
2017-10-08 22:27:12 +0300

    rezultat final patrat - miscare stanga , dreapta , sus , jos cu succes

--- LABORATOR 01/lab_01_temaPatrat/MoveSquare.java
+++ LABORATOR 01/lab_01_temaPatrat/MoveSquare.java
@@ -49,7 +49,7 @@ public class MoveSquare {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 478, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel playingGround = new JPanel();
@@ -65,33 +65,67 @@ public class MoveSquare {
 		playingGround.add(nr_clicks);
 		
 		final JLabel lblWidth = new JLabel("Width:");
-		lblWidth.setBounds(288, 215, 46, 14);
+		lblWidth.setBounds(122, 215, 46, 14);
 		playingGround.add(lblWidth);
 		
 		final JLabel lblHeight = new JLabel("Height:");
-		lblHeight.setBounds(288, 236, 46, 14);
+		lblHeight.setBounds(122, 236, 46, 14);
 		playingGround.add(lblHeight);
 		
 		final JLabel width = new JLabel("");
-		width.setBounds(344, 215, 46, 14);
+		width.setBounds(165, 215, 46, 14);
 		playingGround.add(width);
 		
 		final JLabel height = new JLabel("");
-		height.setBounds(344, 236, 46, 14);
+		height.setBounds(165, 236, 46, 14);
 		playingGround.add(height);
 		
 		nr_clickVar = 0;
 		
 		final Canvas canvas = new Canvas();
-		canvas.setBounds(10, 10, 414, 199);
+		canvas.setBounds(10, 10, 442, 199);
 		
+		
+		
+		final JLabel mouseX = new JLabel("");
+		mouseX.setBounds(274, 215, 46, 14);
+		playingGround.add(mouseX);
+		
+		final JLabel lblMousex = new JLabel("MouseX:");
+		lblMousex.setBounds(221, 215, 55, 14);
+		playingGround.add(lblMousex);
+		
+		final JLabel lblMousey = new JLabel("MouseY:");
+		lblMousey.setBounds(221, 236, 55, 14);
+		playingGround.add(lblMousey);
+		
+		final JLabel mouseY = new JLabel("");
+		mouseY.setBounds(274, 236, 46, 14);
+		playingGround.add(mouseY);
+		
+		final JLabel xTravel = new JLabel("XTravel(px):");
+		xTravel.setBounds(331, 215, 75, 14);
+		playingGround.add(xTravel);
+		
+		final JLabel label_2 = new JLabel("");
+		label_2.setBounds(406, 215, 46, 14);
+		playingGround.add(label_2);
+		
+		final JLabel label_3 = new JLabel("");
+		label_3.setBounds(406, 236, 46, 14);
+		playingGround.add(label_3);
+		
+		final JLabel yTravel = new JLabel("YTravel(px):");
+		yTravel.setBounds(331, 236, 75, 14);
+		playingGround.add(yTravel);
+
 		final int width_var = canvas.getWidth();
 		final int height_var = canvas.getHeight();
-		System.out.println("width_var: "+canvas.getWidth());
-		System.out.println("height_var: "+canvas.getHeight());
+
+		width.setText(Integer.toString(canvas.getWidth()));
+		height.setText(Integer.toString(canvas.getHeight()));
 		
-		width.setText(Integer.toString(width_var));
-		height.setText(Integer.toString(height_var));
+		
 		
 		canvas.setBackground(Color.LIGHT_GRAY);
 		canvas.addMouseListener(new MouseAdapter() {
@@ -99,6 +133,14 @@ public class MoveSquare {
 				
 				nr_clickVar++;
 				nr_clicks.setText(Integer.toString(nr_clickVar));
+				
+				mouseX.setText(Integer.toString(e.getX()));
+				mouseY.setText(Integer.toString(e.getY()));
+				
+				label_2.setText(Integer.toString(width_var -e.getX()));
+				label_3.setText(Integer.toString(height_var-e.getY()));
+
+				
 				Graphics g = canvas.getGraphics();
 				canvas.paint(g);
 				
@@ -119,7 +161,7 @@ public class MoveSquare {
 						canvas.paint(g);
 						g.fillRect( x_rand+ drp,  y_rand, 25, 25);
 						try {
-							Thread.sleep(10);
+							Thread.sleep(5);
 						} catch (InterruptedException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
@@ -132,7 +174,7 @@ public class MoveSquare {
 						canvas.paint(g);
 						g.fillRect( x_rand,  y_rand+ jos, 25, 25);
 						try {
-							Thread.sleep(10);
+							Thread.sleep(5);
 						} catch (InterruptedException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
@@ -142,11 +184,11 @@ public class MoveSquare {
 				case 2:
 					System.out.println("case 3 stanga ");
 					
-					for (int stg=0; stg <= width_var-25-e.getX(); stg++) {
+					for (int stg=0; stg <= e.getX(); stg++) {
 						canvas.paint(g);
 						g.fillRect( x_rand - stg,  y_rand, 25, 25);
 						try {
-							Thread.sleep(10);
+							Thread.sleep(5);
 						} catch (InterruptedException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
@@ -155,11 +197,11 @@ public class MoveSquare {
 					break;
 				case 3:
 					System.out.println("case 4 sus");
-					for (int sus=0; sus <= width_var-25-e.getX(); sus++) {
+					for (int sus=0; sus <= e.getY(); sus++) {
 						canvas.paint(g);
-						g.fillRect( x_rand ,  y_rand- sus, 25, 25);
+						g.fillRect( x_rand ,  y_rand-sus, 25, 25);
 						try {
-							Thread.sleep(10);
+							Thread.sleep(5);
 						} catch (InterruptedException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
@@ -181,7 +223,8 @@ public class MoveSquare {
 		});
 		
 		playingGround.add(canvas);
-
+		
+		
 		
 	}
 	 
